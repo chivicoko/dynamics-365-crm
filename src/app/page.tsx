d6d40cd630f03c3c844dbcc092f9175ff57682e5
chivicoko <victor.c.okoye@gmail.com>
2024-12-21 05:58:52 +0100

    update.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,9 @@ export default function Home() {
         <main className="w-full lg:w-[87vw] min-h-screen pb-16 border-l-2">
             <Navbar />
             <section className='py-3 px-4'>
-                <div className='py-5 px-6 bg-white rounded-[13px] shadow-xl'>
-                    Welcome to the Home page. Click the &apos;Leads&apos; tab on the sidebar
+                <div className='py-5 px-6 bg-white rounded-[13px] shadow-xl text-center'>
+                    <h1 className='mb-5 text-3xl'>Welcome to the Home page</h1>
+                    <p>Click the &apos;Leads&apos; tab on the left sidebar</p>
                 </div>
             </section>
         </main>
