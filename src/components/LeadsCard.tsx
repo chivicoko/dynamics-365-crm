97aed7aaee43bfcce51e91379be9f0f7cc7b6f46
chivicoko <victor.c.okoye@gmail.com>
2024-12-21 05:53:44 +0100

    update lead card style

--- src/components/LeadsCard.tsx
+++ src/components/LeadsCard.tsx
@@ -84,7 +84,7 @@ const LeadsCard: React.FC<leadModalProps> = ({ handleLeadToggle }) => {
                                 </div>
                                 <div className="flex flex-col gap-1">
                                     <p className='text-xs font-semibold'>{leadsCard.name}</p>
-                                    <div className='text-xs text-gray-500 flex items-center gap-1'>
+                                    <div className='text-xs text-gray-500 flex items-center flex-wrap gap-1'>
                                         {leadsCard.position}
                                         <p className='w-1 h-1 bg-gray-500 rounded-full'></p>
                                         {leadsCard.at}
